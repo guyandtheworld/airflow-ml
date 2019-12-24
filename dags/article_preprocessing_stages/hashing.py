35a5a6de4eef3dec93ae1290a729f8007437087f
Adarsh S <4darshofficial@gmail.com>
2019-12-24 14:01:26 +0530

    config structure

--- dags/article_preprocessing_stages/hashing.py
+++ dags/article_preprocessing_stages/hashing.py
@@ -1,2 +1,19 @@
+from google.cloud import storage
+from google.cloud.storage import Blob
+
+
+def index_company():
+
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs("raw-alrt-ai")
+
+    entities_tracked = set([x.name.split("/")[0] for x in blobs])
+    print(entities_tracked)
+
+
 def greet():
     print('Writing in file')
+
+
+if __name__ == "__main__":
+    index_company()
---
+++ script/dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source env/bin/activate
+export GOOGLE_APPLICATION_CREDENTIALS="/home/powerboi/rey/config/google_cloud_key.json"
