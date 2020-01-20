12540dded6553016a785a334f61b18ba2ff6dc21
Adarsh S <4darshofficial@gmail.com>
2020-01-20 18:32:29 +0530

    more efficient method

--- utils/extraction/body_extraction.py
+++ utils/extraction/body_extraction.py
@@ -1,10 +1,11 @@
 import requests
 import urllib3
+import time
 import warnings
+import concurrent.futures
 
+import pandas as pd
 from dragnet import extract_content
-from threading import Thread
-from multiprocessing import Pool
 
 from data.article import Article
 from data.title_analytics import TitleAnalytics
@@ -15,37 +16,51 @@ def warn(*args, **kwargs):
     pass
 
 
+out = []
+CONNECTIONS = 100
+TIMEOUT = 5
+
 warnings.warn = warn
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def gen_text_dragnet(article):
-    print(article.url)
-    try:
-        r = requests.get(article.url, verify=False)
-        content = extract_content(r.content)
-    except ConnectionError as e:
-        print(e)
-        content = ""
-    except Exception as e:
-        print(e)
-        content = ""
-    # save only this much or we run out of mongo space
+def gen_text_dragnet(article, timeout):
+    r = requests.get(article.url, verify=False)
+    content = extract_content(r.content)
     article.update(body=content[:500])
 
 
 def extract_body():
+    """
+    the problem probably is that we're running out of
+    memory. so we'll run this process once every 30 minutes
+    and only process about 2k articles
+    """
     global_init()
     try:
         articles = Article.objects.filter(
-            body__exists=False)[:1000]
+            body__exists=False)[:500]
     except Exception as e:
         print(e)
         raise
+
     print("extracting entities from {} articles".format(len(articles)))
 
-    p = Pool(20)
-    p.map(gen_text_dragnet, articles)
-    p.terminate()
-    p.join()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
+        future_to_url = (executor.submit(gen_text_dragnet, article, TIMEOUT)
+                         for article in articles)
+        time1 = time.time()
+        for future in concurrent.futures.as_completed(future_to_url):
+            try:
+                data = future.result()
+            except Exception as exc:
+                data = str(type(exc))
+            finally:
+                out.append(data)
+                print(str(len(out)), end="\r")
+
+        time2 = time.time()
+
+    print(f'Took {time2-time1:.2f} s')
+    print(pd.Series(out).value_counts())
