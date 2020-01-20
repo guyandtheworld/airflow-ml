2c13135aacbc5c4d0126810ef41b8e9f2ac3467a
Adarsh S <4darshofficial@gmail.com>
2020-01-20 14:38:50 +0530

    changes for body extraction

--- utils/extraction/body_extraction.py
+++ utils/extraction/body_extraction.py
@@ -4,6 +4,7 @@ import warnings
 
 from dragnet import extract_content
 from threading import Thread
+from multiprocessing import Pool
 
 from data.article import Article
 from data.title_analytics import TitleAnalytics
@@ -19,41 +20,32 @@ warnings.warn = warn
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def gen_text_dragnet(*articles):
-    for article in articles:
-        print(article.url)
-        try:
-            r = requests.get(article.url, verify=False)
-            content = extract_content(r.content)
-        except ConnectionError as e:
-            print(e)
-            content = ""
-        except Exception as e:
-            print(e)
-            content = ""
-        # save only this much or we run out of mongo space
-        article.update(body=content[:500])
+def gen_text_dragnet(article):
+    print(article.url)
+    try:
+        r = requests.get(article.url, verify=False)
+        content = extract_content(r.content)
+    except ConnectionError as e:
+        print(e)
+        content = ""
+    except Exception as e:
+        print(e)
+        content = ""
+    # save only this much or we run out of mongo space
+    article.update(body=content[:500])
 
 
 def extract_body():
     global_init()
     try:
         articles = Article.objects.filter(
-            body__exists=False)[:20000]
+            body__exists=False)[:1000]
     except Exception as e:
         print(e)
         raise
+    print("extracting entities from {} articles".format(len(articles)))
 
-    for i in range(0, len(articles), 100):
-        print("processed: {}".format(i))
-        threads = []
-
-        for slide in range(i, i+100, 10):
-            # create 2 threads with 10 requests each
-            process = Thread(target=gen_text_dragnet,
-                             args=articles[slide:slide+10])
-            process.start()
-            threads.append(process)
-
-        for process in threads:
-            process.join()
+    p = Pool(20)
+    p.map(gen_text_dragnet, articles)
+    p.terminate()
+    p.join()
